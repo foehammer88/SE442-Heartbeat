17eced9898f029f5701bf6af472b48fc73546337
foehammer88 <foehammer88@gmail.com>
2012-11-01 18:44:54 -0400

    Added NurseStation

---
+++ Heartbeat/src/NurseStation.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Patrick
+ *
+ */
+public class NurseStation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am a nurse.");
+	}
+
+}
