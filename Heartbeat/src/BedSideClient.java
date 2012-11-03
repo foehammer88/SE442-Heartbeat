21da50814713915669010a2e4c99ba5e204c9158
shunnymok <sxb5828@rit.edu>
2012-11-03 11:16:38 -0400

    Adding class which is the Remote Interface for BedSideClientImpl

--- Heartbeat/src/BedSideClient.java
+++ Heartbeat/src/BedSideClient.java
@@ -2,8 +2,18 @@
  * 
  */
 
-public class BedSideClient {
-	public void testmethod() { 
+import java.rmi.Remote; 
+import java.rmi.RemoteException;
+
+public interface BedSideClient {
+	
+	public void addBedSideListener() throws RemoteException;
 		
-	}
+	public void removeBedSideListener() throws RemoteException;
+	
+	public void alarmAcknowledged() throws RemoteException;
+	
+	public void getPatientHistoryInfo() throws RemoteException; 
+	
+	public void getPatientTrendInfo() throws RemoteException; 
 }
