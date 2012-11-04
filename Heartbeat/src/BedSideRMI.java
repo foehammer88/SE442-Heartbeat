dd59b892103bbc0da0a1b0d6c15244011dafb7c3
shunnymok <sxb5828@rit.edu>
2012-11-04 13:50:09 -0500

    Added additional code

--- Heartbeat/src/BedSideRMI.java
+++ Heartbeat/src/BedSideRMI.java
@@ -3,9 +3,11 @@ import java.rmi.RemoteException;
 
 public interface BedSideRMI extends Remote{
 
-	public void getPatientTrendInfo(String patientID) throws RemoteException; 
+	public void getPatientTrendInfo() throws RemoteException; 
 	
 	public void registerPatientToBedSide(String patiendName, String patientID, String nurseRegistryID, String admitDate) throws RemoteException; 
 
 	public void dischargePatientFromBedSide(String patientID, String dischargeDate) throws RemoteException; 
+	
+	public void alarmAcknowledged() throws RemoteException;  
 }
