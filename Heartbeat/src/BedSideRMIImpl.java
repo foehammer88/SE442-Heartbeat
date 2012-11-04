7799985cdda574fd3f2c7906409bf76a87283818
shunnymok <sxb5828@rit.edu>
2012-11-04 13:50:29 -0500

    Removed a patientID paramater being passed in

--- Heartbeat/src/BedSideRMIImpl.java
+++ Heartbeat/src/BedSideRMIImpl.java
@@ -8,17 +8,29 @@ public class BedSideRMIImpl implements BedSideRMI{
 
 	private String nurseRegistryID;
 	private String rmiRegistryName = "Bed-Side-RMI";
+	private BedInterface bedInterface;
 	
-	public synchronized void getPatientTrendInfo(String patientID) throws RemoteException { 
+	public BedSideRMIImpl() { 
 		
+		bedInterface = new BedInterface(); 
+	}
+	
+	public synchronized void getPatientTrendInfo() throws RemoteException { 
+		
+		bedInterface.getPatientTrendDate();
 	}
 	
 	public synchronized void registerPatientToBedSide(String patientName, String patientID, String nurseRegistryID, String admitDate) throws RemoteException { 
 		
-		
+		bedInterface.registerPatient(patientName, patientID, admitDate);
 	}
 	
 	public synchronized void dischargePatientFromBedSide(String patientId, String dischargeDate) throws RemoteException { 
+		
+		bedInterface.dischargePatient(patientId, dischargeDate);
+	}
+	
+	public synchronized void alarmAcknowledged() throws RemoteException { 
 		
 	}
 	
