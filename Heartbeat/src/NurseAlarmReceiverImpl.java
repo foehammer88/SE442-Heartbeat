d9c26e691f387f9064621376f1460ae55cfe451f
shunnymok <sxb5828@rit.edu>
2012-11-04 11:02:00 -0500

    Adding NurseSide RMI

---
+++ Heartbeat/src/NurseAlarmReceiverImpl.java
@@ -0,0 +1,25 @@
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+
+public class NurseAlarmReceiverImpl implements NurseAlarmReceiver{
+	
+	private static final String rmiRegistryName = "Nurse-Alarm-Receiver";
+	
+	public void alarmRaised(String alarm, String patientID) throws RemoteException{ 
+		
+		System.out.println("Alarm has been raised: " + alarm);
+	}
+	
+	public void bindToRegistry() throws RemoteException, MalformedURLException { 
+		System.out.println("Binding nurse alarm to RMI registry");
+		NurseAlarmReceiverImpl nurseAlarm = new NurseAlarmReceiverImpl(); 
+		Naming.rebind(rmiRegistryName, nurseAlarm);
+	}
+	
+	public void unBindToRegistry() throws RemoteException, MalformedURLException, NotBoundException { 
+		System.out.println("Unbinding nurse alarm server from registry");
+		Naming.unbind(rmiRegistryName);
+	}
+}
