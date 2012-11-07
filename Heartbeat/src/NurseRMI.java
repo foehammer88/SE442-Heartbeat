aa343c9f5321e6cf8aa8f7de2c8d00a360fcf6e4
shunnymok <sxb5828@rit.edu>
2012-11-07 18:48:32 -0500

    Updated code

--- Heartbeat/src/NurseRMI.java
+++ Heartbeat/src/NurseRMI.java
@@ -4,4 +4,6 @@ import java.rmi.RemoteException;
 public interface NurseRMI extends Remote {
 	
 	public void alarmRaised(AlarmPair alarmObject) throws RemoteException;
+	
+	public void receivePatientVitals(String bp, String hr, String temp, String rr) throws RemoteException;
 }
