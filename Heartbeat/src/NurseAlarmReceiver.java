bcd1b88266b817323ea154bc2f71cb617768e881
shunnymok <sxb5828@rit.edu>
2012-11-07 18:36:31 -0500

    Removed NurseAlarmReceiver and NurseAlarmReceiverIMpl

--- Heartbeat/src/NurseAlarmReceiver.java
+++
@@ -1,7 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface NurseAlarmReceiver extends Remote {
-	
-	public void alarmRaised(AlarmPair alarmObject) throws RemoteException;
-}
